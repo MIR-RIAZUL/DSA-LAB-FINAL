f1f156c63dde1b1b497706ebc2729eaf11776cf1
MIR-RIAZUL <mirriazul859@gmail.com>
2025-10-13 19:15:12 +0600

    nothings

--- BFS/bfs.cpp
+++ BFS/bfs.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
 #include<vector>
-#include<queue>
-
 using namespace std;
+
+void addEdge(vector<int> adj[], int src, int des)
+{
+    adj[src].push_back(des);
+}
 
 
 
 int main()
 {
+    int V = 6;
+    vector<int> adjList[V];
 
-}
+    addEdge(adjList,0,1);
+    addEdge(adjList,0,4);
+    addEdge(adjList,1,2);
+    addEdge(adjList,1,3);
+    addEdge(adjList,1,4);
+    addEdge(adjList,3,4);
+    addEdge(adjList,3,5);
+
+} 
