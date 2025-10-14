af7bd2dc1012edff7a6841c461e8d4aebf3e180a
MIR-RIAZUL <mirriazul859@gmail.com>
2025-10-14 16:24:13 +0600

    inorder traversal

--- BST/myCode.cpp
+++ BST/myCode.cpp
@@ -1,51 +1,62 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 struct node
 {
     int data;
-    node *left,*right;
-    
+    node *left, *right;
 };
 
-node *main_root=NULL;
+node *main_root = NULL;
 
 node *createNode(int x)
 {
-    node *temp=new node();
-    temp->data=x;
-    temp->right=NULL;
-    temp->left=NULL;
+    node *temp = new node();
+    temp->data = x;
+    temp->right = NULL;
+    temp->left = NULL;
     return temp;
 }
 
-node *insertNode( node *root,int key)
+node *insertNode(node *root, int key)
 {
-    if(root==NULL)
+    if (root == NULL)
     {
         return createNode(key);
     }
     else if (root->data < key)
     {
-        root->right=insertNode(root->right,key);
+        root->right = insertNode(root->right, key);
     }
-    else if (root->data>key)
+    else if (root->data > key)
     {
-        root->left=insertNode(root->left,key);
+        root->left = insertNode(root->left, key);
     }
-    
-    
+    return root;
 }
 
+// this is inorder traversal
+
+void inorder(node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
 
 int main()
 {
-    main_root=insertNode(main_root,6);
-    insertNode(main_root,4);
-    insertNode(main_root,7);
-    insertNode(main_root,2);
-    insertNode(main_root,5);
-    insertNode(main_root,8);
-    insertNode(main_root,3);
+    main_root = insertNode(main_root, 6);
+    insertNode(main_root, 4);
+    insertNode(main_root, 7);
+    insertNode(main_root, 2);
+    insertNode(main_root, 5);
+    insertNode(main_root, 8);
+    insertNode(main_root, 3);
 
+    cout << "inorder traversal:";
+    inorder(main_root);
 }
