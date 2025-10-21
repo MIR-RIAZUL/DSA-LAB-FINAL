5c00a3c5dbdce4f1816486a28548c2f65fe54717
MIR-RIAZUL <mirriazul859@gmail.com>
2025-10-22 02:22:28 +0600

    assignment task 2

--- assignment 1/Task2.cpp
+++ assignment 1/Task2.cpp
@@ -1,1 +1,44 @@
 #include<iostream>
+#include<stack>
+using namespace std;
+
+bool palindrome(string s)
+{
+    stack<int>st;
+    int n=s.length();
+
+    for(int i=0;i<n;i++)
+    {
+        st.push(s[i]);
+    }
+    string reverse;
+    while(!st.empty())
+    {
+        reverse+=st.top();
+        st.pop();
+    }
+    return s==reverse;
+}
+
+int main()
+{
+    int n;
+    cout<<"how many times you want to test:";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<"enter the word:";
+        string s;
+        cin>>s;
+        if(palindrome(s))
+        {
+            cout<<"yes"<<endl;
+        }
+        else 
+        {
+            cout<<"no"<<endl;
+        }
+    }
+
+}
